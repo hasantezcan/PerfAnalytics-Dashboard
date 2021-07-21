f73046ab57cf49decbc4ed1f3fab09e8a8b8d7cf
hasantezcan <hasantezcan77@gmail.com>
2021-07-22 02:28:14 +0300

    test: add call handleTimeRange function test

--- src/components/TimeRangeFilter/index.spec.tsx
+++ src/components/TimeRangeFilter/index.spec.tsx
@@ -1,11 +1,24 @@
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import sinon, { SinonSpy } from 'sinon'
 import TimeRangeFilter from '.'
+
+const sandbox = sinon.createSandbox()
 
 describe('TimeRangeFilter specs', () => {
   const handleTimeRange = (start: any, end: any) => {
     console.log(`start`, start)
     console.log(`end`, end)
   }
+
+  let handleTimeRangeSpy: SinonSpy
+
+  beforeEach(() => {
+    handleTimeRangeSpy = sandbox.spy(handleTimeRange)
+  })
+
+  afterEach(() => {
+    sandbox.verifyAndRestore()
+  })
 
   it('Should render time range control form ', () => {
     const { container } = render(
@@ -15,30 +28,46 @@ describe('TimeRangeFilter specs', () => {
     expect(container.getElementsByClassName('ant-form').length).toBe(1)
   })
 
-  // it('Should render date picker when click on it', () => {
-  //   const { container } = render(
-  //     <TimeRangeFilter setTimeRange={handleTimeRange} />
-  //   )
+  it('Should render date picker when click on it', () => {
+    const { container } = render(
+      <TimeRangeFilter setTimeRange={handleTimeRange} />
+    )
 
-  //   const datePicker = container.getElementsByClassName('ant-picker-input')[0]
+    const datePicker = container.getElementsByClassName('ant-picker-input')[0]
 
-  //   fireEvent.click(datePicker)
+    fireEvent.click(datePicker)
+    waitFor(() => {
+      expect(
+        container.getElementsByClassName('ant-picker-dropdown').length
+      ).toBe(1)
+    })
+  })
 
-  //   expect(container.getElementsByClassName('ant-picker-dropdown').length).toBe(
-  //     1
-  //   )
-  // })
+  it('Should render time picker when click on it', () => {
+    const { container } = render(
+      <TimeRangeFilter setTimeRange={handleTimeRange} />
+    )
 
-  // it('Should render time picker when click on it', () => {
-  //   const { container } = render(
-  //     <TimeRangeFilter setTimeRange={handleTimeRange} />
-  //   )
+    const timePicker = container.getElementsByClassName('ant-picker-range')[0]
+    fireEvent.click(timePicker)
 
-  //   const timePicker = container.getElementsByClassName('ant-picker-range')[0]
-  //   fireEvent.click(timePicker)
+    waitFor(() => {
+      expect(
+        container.getElementsByClassName('ant-picker-panel-container').length
+      ).toBe(1)
+    })
+  })
 
-  //   expect(
-  //     container.getElementsByClassName('ant-picker-panel-container').length
-  //   ).toBe(1)
-  // })
+  it('Should call handleTimeRange function', () => {
+    const { container } = render(
+      <TimeRangeFilter setTimeRange={handleTimeRange} />
+    )
+
+    const button = container.getElementsByClassName('ant-btn')[0]
+    fireEvent.click(button)
+
+    waitFor(() => {
+      expect(handleTimeRangeSpy.calledOnce).toBe(true)
+    })
+  })
 })
