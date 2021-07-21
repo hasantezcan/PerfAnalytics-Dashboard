979d4b6330021ebfbfb840eb77de9f68dd34bae7
hasantezcan <hasantezcan77@gmail.com>
2021-07-21 18:27:15 +0300

    refactor: update urlFilter empty view

--- src/components/UrlFilter/index.tsx
+++ src/components/UrlFilter/index.tsx
@@ -1,5 +1,5 @@
-import { Checkbox, Typography, Form } from 'antd'
-import { getClippedText } from '~/util/text.utils'
+import { Checkbox, Typography, Form, Empty, Button } from 'antd'
+import { getClippedText } from '../../util/text.utils'
 
 const { Title } = Typography
 const CheckboxGroup = Checkbox.Group
@@ -28,25 +28,48 @@ function UrlFilter({ urls, onSelect, selectedUrls }: UrlFilterProps) {
     <Form>
       <Title level={3}>Select URL</Title>
 
-      <Form.Item labelCol={{ span: 6, offset: 6 }}>
-        <Checkbox
-          indeterminate={
-            selectedUrls.length > 0 && selectedUrls.length !== urls.length
-          }
-          onChange={onCheckAllChange}
-          checked={selectedUrls.length > 0}
-        >
-          Check all
-        </Checkbox>
-      </Form.Item>
+      {urls.length > 0 ? (
+        <>
+          <Form.Item labelCol={{ span: 6, offset: 6 }}>
+            <Checkbox
+              indeterminate={
+                selectedUrls.length > 0 && selectedUrls.length !== urls.length
+              }
+              onChange={onCheckAllChange}
+              checked={selectedUrls.length > 0}
+            >
+              Check all
+            </Checkbox>
+          </Form.Item>
 
-      <Form.Item>
-        <CheckboxGroup
-          options={urlOptions}
-          value={selectedUrls}
-          onChange={onChange}
-        />
-      </Form.Item>
+          <Form.Item>
+            <CheckboxGroup
+              options={urlOptions}
+              value={selectedUrls}
+              onChange={onChange}
+            />
+          </Form.Item>
+        </>
+      ) : (
+        <Empty
+          image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+          imageStyle={{
+            height: 60
+          }}
+          description={
+            <span>There is not metric found in this time range</span>
+          }
+        >
+          <Button
+            href="https://github.com/hasantezcan/PerfAnalytics-Dashboard#:~:text=You%20can%20create%20analytics%20metric%20data%20from%20those%20sites:"
+            target="_blank"
+            rel="noreferrer"
+            type="primary"
+          >
+            Create Now
+          </Button>
+        </Empty>
+      )}
     </Form>
   )
 }
